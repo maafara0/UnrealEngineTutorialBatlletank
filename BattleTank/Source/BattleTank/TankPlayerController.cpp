174f2d3e1a4b5ad1553035c40b14d827b8a84530
Ostad <mapjiv@gmail.com>
2020-07-16 21:25:02 +0430

    Tank posses start

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,7 +3,23 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not possesing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("player controller possesing %s"), *ControlledTank->GetName())
+	}
+	UE_LOG(LogTemp, Warning, TEXT("player controller beign "))
+}
 ATank* ATankPlayerController::GetControlledTank() const
 {
+	UE_LOG(LogTemp, Warning, TEXT("player controller beign play"))
 	return Cast<ATank>(GetPawn());
 }
