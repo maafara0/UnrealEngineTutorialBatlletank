baed5bdea65daabf9074b69b4534cc5b662fd205
Ostad <mapjiv@gmail.com>
2020-07-25 19:46:41 +0430

    Tank movement buggy working

--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -7,7 +7,6 @@ void UTankTrack::SetThrottle(float Throttle)
 {
 	//Throttle = FMath::Clamp<float>(Throttle, 0, 1);
 	auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%S Throttle value: %f"), *Name, Throttle)
 
 	auto ForceApllied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
